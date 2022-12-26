a6883dd3600e9494540942d5163f257dd5d2cf0d
woundmee <woundmee@gmail.com>
2022-12-26 23:44:36 +0700

    first commit

---
+++ CustomAuthAspNet6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomAuthAspNet6.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
